bb57dd464e962af3d00d841235cf6816964ece40
Mateus Machado Gonzalez <mateusmgonzalez@gmail.com>
2019-04-25 20:55:21 -0300

    Començando um projeto em Node e Restify

---
+++ dist/server/server.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const environment_1 = require("../common/environment");
+class Server {
+    initRoutes() {
+        return new Promise((resolve, reject) => {
+            try {
+                this.application = restify.createServer({
+                    name: 'meat-api',
+                    version: '1.0.0'
+                });
+                this.application.use(restify.plugins.queryParser());
+                this.application.get('/info', [(req, res, next) => {
+                        if (req.userAgent && req.userAgent().includes('MSIE 7.0')) {
+                            let error = new Error();
+                            error.statusCode = 400;
+                            error.message = "Please, update your browser";
+                            return next(error);
+                        }
+                        return next();
+                    }, (req, resp, next) => {
+                        resp.json({
+                            broser: req.userAgent(),
+                            method: req.method,
+                            url: req.href(),
+                            path: req.path(),
+                            query: req.query
+                        });
+                        return next();
+                    }]);
+                this.application.listen(environment_1.environment.server.port, () => {
+                    resolve(this.application);
+                });
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+    bootstrap() {
+        return this.initRoutes().then(() => this);
+    }
+}
+exports.Server = Server;
